2a881acaece0246101804c0347022ad8f2eeeab4
Hunter <hmvitous@yahoo.com>
2020-04-22 17:27:30 +0200

    Adds viaplay api url to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  async componentDidMount() {
+    const url = "https://content.viaplay.se/pc-se/serier/samtliga"
+    const response = await fetch(url)
+    const data = await response.json()
+  }
+
+  render() {
+    return (
+    )
+  }
 }
-
 export default App;
